8368b4c7186d21337977ce75a98ed9f44f1a2626
pop <pop929@qq.com>
2020-07-01 19:16:13 +0800

    master

---
+++ temp.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: pop
+ * @E-mail: pop929@qq.com
+ * @Date: 2020-07-01 17:10:20
+ * @Version: 1.0.1
+ * @LastEditTime: 2020-07-01 18:52:05
+ */ 
+
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    std::cout<<"你好VS Code！！！"<<std::endl;
+    std::system("pause");
+    return 0;
+}
